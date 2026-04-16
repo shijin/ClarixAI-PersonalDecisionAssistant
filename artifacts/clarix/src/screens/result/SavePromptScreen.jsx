6b92ca6590aa1dcdd3b612744f820d793bab282c
Shijin Ramesh <kshijin92@gmail.com>
2026-04-16 07:32:50 +0000

    Updated save prompt screen

--- artifacts/clarix/src/screens/result/SavePromptScreen.jsx
+++ artifacts/clarix/src/screens/result/SavePromptScreen.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDecision } from "../../hooks/useDecision";
 import { useUser } from "../../context/UserContext";
